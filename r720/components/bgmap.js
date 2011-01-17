d6f4af40b9b78260ef41f71fb4b01b338f0127d9
Donald Carr <donald.carr@nokia.com>
2011-01-17 10:28:20 +0000

    Ack, add excluded bgmap.js file

---
+++ r720/components/bgmap.js
@@ -0,0 +1,12 @@
+.pragma library
+
+var bgmap = new Array();
+
+bgmap['music'] = "music.jpg"
+bgmap['video'] = "videos.jpg"
+bgmap['scripts'] = "programs.jpg"
+bgmap['weather'] = "weather.jpg"
+bgmap['picture'] = "pictures.jpg"
+bgmap['programs'] = "programs.jpg"
+bgmap['system'] = "system.jpg"
+bgmap['dashboard'] = "programs.jpg"
