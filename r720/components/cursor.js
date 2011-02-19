ac240cb941670bbc6dfcf22287cd965b9d4afe5f
Girish Ramakrishnan <girish@forwardbias.in>
2011-02-19 18:04:36 +0530

    Add mising file

---
+++ r720/components/cursor.js
@@ -0,0 +1,6 @@
+function initialize() {
+    cursor.idleTimeout = 2
+    cursor.defaultCursorPath = themeResourcePath + "/media/pointer-focus.png"
+    cursor.clickedCursorPath = themeResourcePath + "/media/pointer-focus-click.png"
+}
+
