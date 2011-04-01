65c0e9be0c36a132b26aa768eec795c0ffbec291
Donald Carr <donald.carr@nokia.com>
2011-04-01 12:54:05 +0000

    Guard against absence of cursor plugin

--- r720/components/cursor.js
+++ r720/components/cursor.js
@@ -1,6 +1,8 @@
 function initialize() {
-    cursor.idleTimeout = 2
-    cursor.defaultCursorPath = themeResourcePath + "/media/pointer-focus.png"
-    cursor.clickedCursorPath = themeResourcePath + "/media/pointer-focus-click.png"
+    if (typeof(cursor) != 'undefined') {
+        cursor.idleTimeout = 2
+        cursor.defaultCursorPath = themeResourcePath + "/media/pointer-focus.png"
+        cursor.clickedCursorPath = themeResourcePath + "/media/pointer-focus-click.png"
+    }
 }
 
