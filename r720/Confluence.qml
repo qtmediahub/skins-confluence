qtmediahub/skins-confluence

commit dddd07a8622822ac5d459a8d60b12cb9435365f9

Destroy views when closed

r720/Confluence.qml
@@ -106,6 +106,11 @@ FocusScope {
     function show(element) {
         if (_selectedElement && _selectedElement != element)
             _selectedElement.state = ""
+            if (_selectedElement.deleteOnClose) {
+                rootMenuModel.get(_selectedIndex).visualElement = null
+                _selectedElement = null
+                _selectedIndex = 0
+            }
 
         if (element == mainBlade) {
             state = ""
@@ -264,16 +269,16 @@ FocusScope {
 
         // ## QML Bug : without background: <something> on the first item background breaks
         var rootMenuItems = [
-            { name: qsTr("App Store"), engine: "appstore", role: QMHPlugin.Store, sourceUrl: "AppStoreWindow.qml", background: null },
-            { name: qsTr("Dashboard"), role: QMHPlugin.Dashboard, sourceUrl: "DashboardWindow.qml", background: "programs.jpg" },
-            { name: qsTr("Music"), engine: "music", role: QMHPlugin.Music, sourceUrl: "MusicWindow.qml", background: "music.jpg" },
-            { name: qsTr("Picture"), engine: "picture", role: QMHPlugin.Picture, sourceUrl: "PictureWindow.qml", background: "pictures.jpg" },
-            { name: qsTr("Video"), engine: "video", role: QMHPlugin.Video, sourceUrl: "VideoWindow.qml", background: "videos.jpg" },
+            { name: qsTr("App Store"), engine: "appstore", role: QMHPlugin.Store, sourceUrl: "AppStoreWindow.qml", background: null, constructorArgs: { deleteOnClose: true } },
+            { name: qsTr("Dashboard"), role: QMHPlugin.Dashboard, sourceUrl: "DashboardWindow.qml", background: "programs.jpg", constructorArgs: { deleteOnClose: true } },
+            { name: qsTr("Music"), engine: "music", role: QMHPlugin.Music, sourceUrl: "MusicWindow.qml", background: "music.jpg",  constructorArgs: { deleteOnClose: true } },
+            { name: qsTr("Picture"), engine: "picture", role: QMHPlugin.Picture, sourceUrl: "PictureWindow.qml", background: "pictures.jpg", constructorArgs: { deleteOnClose: true } },
+            { name: qsTr("Video"), engine: "video", role: QMHPlugin.Video, sourceUrl: "VideoWindow.qml", background: "videos.jpg", constructorArgs: { deleteOnClose: true } },
             { name: qsTr("Weather"), role: QMHPlugin.Weather, sourceUrl: "WeatherWindow.qml", visualElement: _weatherWindow, background: "weather.jpg" },
             { name: qsTr("Web"), role: QMHPlugin.Web, sourceUrl: "WebWindow.qml", visualElement: _browserWindow, background: "web.jpg", 
               onActivate: function() { this.initialUrl = "http://www.google.com"; this.enableBrowserShortcuts = true } },
-            { name: qsTr("Remote App"), role: QMHPlugin.Application, sourceUrl: "RemoteAppWindow.qml" },
-            { name: qsTr("Ovi Maps"), role: QMHPlugin.Map, sourceUrl: "MapsWindow.qml", background: "carta_marina.jpg" },
+            { name: qsTr("Remote App"), role: QMHPlugin.Application, sourceUrl: "RemoteAppWindow.qml", constructorArgs: { deleteOnClose: true }},
+            { name: qsTr("Ovi Maps"), role: QMHPlugin.Map, sourceUrl: "MapsWindow.qml", background: "carta_marina.jpg", constructorArgs: { deleteOnClose: true } },
             { name: qsTr("Google Maps"), role: QMHPlugin.Map, sourceUrl: "WebWindow.qml", visualElement: _browserWindow, background: "carta_marina.jpg",
               onActivate: function() { this.initialUrl =  generalResourcePath + "/googlemaps/Nokia.html"; this.enableBrowserShortcuts = false } }
         ]

commit 95b61d6b8e3927135e885acf5f224f9b140e2a76

dddd07a8 broke everything.

r720/Confluence.qml
@@ -104,14 +104,14 @@ FocusScope {
     }
 
     function show(element) {
-        if (_selectedElement && _selectedElement != element)
+        if (_selectedElement && _selectedElement != element) {
             _selectedElement.close()
             if (_selectedElement.deleteOnClose) {
                 rootMenuModel.get(_selectedIndex).visualElement = null
                 _selectedElement = null
                 _selectedIndex = 0
             }
-
+        }
         if (element == mainBlade) {
             state = ""
         } else if(element == avPlayer) {