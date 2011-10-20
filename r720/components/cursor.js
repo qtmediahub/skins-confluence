fd85891dcf0c2566b1717fc637f0d79b58b57c13
Johannes Zellner <johannes.zellner@nokia.com>
2011-10-20 13:22:08 +0200

    dont override mousecursor hide timeout from skin

--- r720/components/cursor.js
+++ r720/components/cursor.js
@@ -1,6 +1,5 @@
 function initialize() {
     if (typeof runtime.cursor  != 'undefined') {
-        runtime.cursor.idleTimeout = 2
         runtime.cursor.defaultCursorPath = themeResourcePath + "/media/pointer-focus.png"
         runtime.cursor.clickedCursorPath = themeResourcePath + "/media/pointer-focus-click.png"
     }
